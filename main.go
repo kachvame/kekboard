dead2264f2e8888bdde5d10bcb78a6d3ff1f631f
invakid404 <invakid404@riseup.net>
2022-04-08 13:11:59 +0300

    feat: send embed

--- main.go
+++ main.go
@@ -49,10 +49,17 @@ func main() {
 		log.Fatalln("`KEKBOARD_CHANNEL_ID` env variable is not set")
 	}
 
+	kekEmoji := os.Getenv("KEK_EMOJI")
+	if kekEmoji == "" {
+		log.Fatalln("`KEK_EMOJI` env variable is not set")
+	}
+
 	client, err := discordgo.New("Bot " + token)
 	if err != nil {
 		log.Fatalln("failed to create discord client:", err)
 	}
+
+	client.Identify.Intents = discordgo.IntentsAll
 
 	db, err := leveldb.OpenFile(dbPath, nil)
 	if err != nil {
@@ -81,7 +88,75 @@ func main() {
 		return []byte(fmt.Sprintf("message-%s-%s", reaction.ChannelID, reaction.MessageID))
 	}
 
-	handleReaction := func(session *discordgo.Session, reaction *discordgo.MessageReaction) {
+	getMessageURL := func(message *discordgo.Message, guildID string) string {
+		return fmt.Sprintf("https://discord.com/channels/%s/%s/%s", guildID, message.ChannelID, message.ID)
+	}
+
+	getMessageEmbed := func(message *discordgo.Message, author *discordgo.Member, guildID string) []*discordgo.MessageEmbed {
+		embeds := make([]*discordgo.MessageEmbed, 0, 4)
+		embeds = append(embeds, &discordgo.MessageEmbed{
+			Author: &discordgo.MessageEmbedAuthor{
+				Name:    author.Nick,
+				IconURL: author.AvatarURL("128"),
+			},
+			Description: message.Content,
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:  "Original",
+					Value: fmt.Sprintf("[Jump!](%s)", getMessageURL(message, guildID)),
+				},
+			},
+		})
+
+		embedIdx := 0
+		for _, attachment := range message.Attachments {
+			if !strings.HasPrefix(attachment.ContentType, "image/") {
+				continue
+			}
+
+			if embedIdx >= len(embeds) {
+				embeds = append(embeds, &discordgo.MessageEmbed{})
+			}
+
+			embeds[embedIdx].URL = "https://twitter.com"
+			embeds[embedIdx].Image = &discordgo.MessageEmbedImage{
+				URL: attachment.URL,
+			}
+
+			embedIdx++
+			if embedIdx >= 4 {
+				break
+			}
+		}
+
+		return embeds
+	}
+
+	putKekboardMessage := func(session *discordgo.Session, message *discordgo.Message, state *kekboardMessageState, guildID string) (*discordgo.Message, error) {
+		guildMember, err := session.GuildMember(guildID, message.Author.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		content := fmt.Sprintf("%s | %d", kekEmoji, state.Reactions)
+		embeds := getMessageEmbed(message, guildMember, guildID)
+
+		if state.MessageID == "" {
+			return session.ChannelMessageSendComplex(state.ChannelID, &discordgo.MessageSend{
+				Content: content,
+				Embeds:  embeds,
+			})
+		}
+
+		return session.ChannelMessageEditComplex(&discordgo.MessageEdit{
+			Channel: state.ChannelID,
+			ID:      state.MessageID,
+			Content: &content,
+			Embeds:  embeds,
+		})
+	}
+
+	handleReaction := func(session *discordgo.Session, reaction *discordgo.MessageReaction, guildID string) {
 		message, err := session.ChannelMessage(reaction.ChannelID, reaction.MessageID)
 		if err != nil {
 			log.Println("received reaction but unable to get message:", reaction.ChannelID, reaction.MessageID)
@@ -92,82 +167,79 @@ func main() {
 		messageKey := getMessageKey(reaction)
 		kekCount := getKekCount(message)
 
-		log.Printf("message %s has %d kek reactions", reaction.MessageID, kekCount)
-
 		hasEnoughReactions := kekCount >= reactionThreshold
 
-		storedState, err := db.Get(messageKey, nil)
-		isOnKekboard := err == nil
+		messageStateBytes, err := db.Get(messageKey, nil)
 
-		log.Println("has enough reactions", hasEnoughReactions, "is on kekboard", isOnKekboard)
+		var messageState *kekboardMessageState
+		if err == nil {
+			messageState = &kekboardMessageState{}
+			if err = json.Unmarshal(messageStateBytes, messageState); err != nil {
+				log.Println("failed to unmarshal state:", err)
 
-		if hasEnoughReactions == isOnKekboard {
-			// TODO: edit message if on kekboard
-
-			return
+				return
+			}
 		}
 
-		if hasEnoughReactions {
-			log.Println("adding to kekboard")
+		isInKekboard := messageState != nil
+		shouldEdit := isInKekboard && messageState.Reactions != kekCount
 
-			message, err := session.ChannelMessageSend(kekboardChannelId, message.Content)
+		if hasEnoughReactions && (!isInKekboard || shouldEdit) {
+			if messageState == nil {
+				messageState = &kekboardMessageState{
+					ChannelID: kekboardChannelId,
+				}
+			}
+
+			messageState.Reactions = kekCount
+
+			message, err := putKekboardMessage(session, message, messageState, guildID)
 			if err != nil {
-				log.Println("failed to send message to kekboard channel:", err)
+				log.Println("failed to put message on kekboard:", err)
 
 				return
 			}
 
-			state := kekboardMessageState{
-				ChannelID: message.ChannelID,
-				MessageID: message.ID,
-				Reactions: kekCount,
-			}
+			messageState.MessageID = message.ID
 
-			marshaledState, err := json.Marshal(state)
+			marshaledState, err := json.Marshal(messageState)
 			if err != nil {
 				log.Println("failed to marshal state:", err)
+
+				return
 			}
 
 			err = db.Put(messageKey, marshaledState, nil)
 			if err != nil {
-				log.Println("failed to add message to kekboard:", err)
+				log.Println("failed to update state in leveldb:", err)
 			}
 
 			return
 		}
 
-		log.Println("removing from kekboard")
+		if isInKekboard && !hasEnoughReactions {
+			err = session.ChannelMessageDelete(messageState.ChannelID, messageState.MessageID)
+			if err != nil {
+				log.Println("failed to delete message from kekboard:", err)
+			}
 
-		state := kekboardMessageState{}
-
-		err = json.Unmarshal(storedState, &state)
-		if err != nil {
-			log.Println("failed to unmarshal stored state:", err)
-
-			return
-		}
-
-		err = session.ChannelMessageDelete(state.ChannelID, state.MessageID)
-		if err != nil {
-			log.Println("failed to delete message:", err)
-		}
-
-		err = db.Delete(messageKey, nil)
-		if err != nil {
-			log.Println("failed to remove message from kekboard:", err)
+			err = db.Delete(messageKey, nil)
+			if err != nil {
+				log.Println("failed to delete state from leveldb:", err)
+			}
 		}
 	}
 
 	client.AddHandler(func(session *discordgo.Session, reactionAdd *discordgo.MessageReactionAdd) {
-		handleReaction(session, reactionAdd.MessageReaction)
+		handleReaction(session, reactionAdd.MessageReaction, reactionAdd.GuildID)
 	})
 
 	client.AddHandler(func(session *discordgo.Session, reactionRemove *discordgo.MessageReactionRemove) {
-		handleReaction(session, reactionRemove.MessageReaction)
+		handleReaction(session, reactionRemove.MessageReaction, reactionRemove.GuildID)
 	})
 
 	client.AddHandler(func(session *discordgo.Session, reactionRemoveAll *discordgo.MessageReactionRemoveAll) {
-		handleReaction(session, reactionRemoveAll.MessageReaction)
+		handleReaction(session, reactionRemoveAll.MessageReaction, reactionRemoveAll.GuildID)
 	})
 
 	if err = client.Open(); err != nil {
