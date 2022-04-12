dead36137587ee8162dc11b690f84e10ed4b3151
invakid404 <invakid404@riseup.net>
2022-04-12 10:23:36 +0300

    chore(dashboard): set up carbon

--- dashboard/src/routes/index.svelte
+++ dashboard/src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "carbon-components-svelte/css/g90.css"
+    import { Button } from "carbon-components-svelte";
+</script>
+
+<Button>Hello, Carbon!</Button>
