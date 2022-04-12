50fcdabc4a08a86a0cc1b5bd80544a353ba41f41
Byanew23 <borislav.yanev23@gmail.com>
2022-04-12 15:27:10 +0300

    add top scorers

--- dashboard/src/routes/index.svelte
+++ dashboard/src/routes/index.svelte
@@ -1,34 +1,39 @@
 <script context="module">
-  export const load = async ({ fetch }) => {
-    const stats = await fetch('/stats').then((res) => res.json());
+    export const load = async ({fetch}) => {
+        const stats = await fetch('/stats').then((res) => res.json());
 
-    return {
-      props: {
-        rows: stats.map((stat) => ({
-          ...stat,
-          id: stat.username,
-        })),
-      },
+        return {
+            props: {
+                topScorers: stats.slice(0, 3),
+                rows: stats.slice(3).map((stat) => ({
+                    ...stat,
+                    username: stat.username.split('#')[0],
+                    id: stat.username,
+                })),
+            },
+        };
     };
-  };
 </script>
 
 <script>
-  import 'carbon-components-svelte/css/g90.css';
-  import { DataTable } from 'carbon-components-svelte';
+    import 'carbon-components-svelte/css/g90.css';
+    import {DataTable} from 'carbon-components-svelte';
+    import Card from '../components/Card.svelte';
 
-  const headers = [
-    {
-      key: 'username',
-      value: 'Username',
-    },
-    {
-      key: 'count',
-      value: 'Count',
-    },
-  ];
+    const headers = [
+        {
+            key: 'username',
+            value: 'Username',
+        },
+        {
+            key: 'count',
+            value: 'Count',
+        },
+    ];
 
-  export let rows;
+    export let rows;
+    export let topScorers;
+
 </script>
-
-<DataTable {headers} {rows} />
+<Card data={topScorers}/>
+<DataTable {headers} {rows}/>
