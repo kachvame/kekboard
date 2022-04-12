deadcf483a6626b31e3db0523d165ca2de891ba4
invakid404 <invakid404@riseup.net>
2022-04-12 10:52:03 +0300

    feat(dashboard): initial stats table

--- dashboard/src/routes/index.svelte
+++ dashboard/src/routes/index.svelte
@@ -1,6 +1,31 @@
-<script>
-    import "carbon-components-svelte/css/g90.css"
-    import { Button } from "carbon-components-svelte";
+<script context="module">
+  export const load = async ({ fetch }) => {
+    const stats = await fetch('http://localhost:8080/stats').then((res) =>
+      res.json(),
+    );
+
+    return {
+      props: { rows: stats },
+    };
+  };
 </script>
 
-<Button>Hello, Carbon!</Button>
+<script>
+  import 'carbon-components-svelte/css/g90.css';
+  import { DataTable } from 'carbon-components-svelte';
+
+  const headers = [
+    {
+      key: 'username',
+      value: 'Username',
+    },
+    {
+      key: 'count',
+      value: 'Count',
+    },
+  ];
+
+  export let rows;
+</script>
+
+<DataTable {headers} {rows} />
