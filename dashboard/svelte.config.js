deadf4e28335cd0ac54bdee8123a1ed0fe2aaea3
invakid404 <invakid404@riseup.net>
2022-04-16 20:23:28 +0300

    chore(dashboard): use `carbon-preprocess-svelte`

--- dashboard/svelte.config.js
+++ dashboard/svelte.config.js
@@ -1,11 +1,12 @@
 import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
+import { optimizeImports } from 'carbon-preprocess-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: [preprocess(), optimizeImports()],
 
   kit: {
     adapter: adapter(),
