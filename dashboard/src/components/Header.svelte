1eaf5ca716945b94d7db70be8903c49bb4b9b9e1
Simo Aleksandrov <simo3003@me.com>
2022-04-16 20:12:21 +0300

    malak-fix: kek header

---
+++ dashboard/src/components/Header.svelte
@@ -0,0 +1,29 @@
+<style lang="scss">
+  @import '@carbon/colors';
+
+  .header {
+    background-color: $gray-100;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+  }
+
+  .header-title {
+    text-align: center;
+  }
+
+  .header-kek {
+    text-align: center;
+  }
+
+  .header-kek > img {
+    height: 100px;
+  }
+</style>
+<header class="header">
+    <div class="header-kek">
+        <img src="kek.jpg" class="header-kek"/>
+    </div>
+    <h1 class="header-title">
+        kekboard
+    </h1>
+</header>
--- dashboard/src/routes/index.svelte
+++ dashboard/src/routes/index.svelte
@@ -1,39 +1,49 @@
 <script context="module">
-  export const load = async ({ fetch }) => {
-    const stats = await fetch('/stats').then((res) => res.json());
+    export const load = async ({fetch}) => {
+        const stats = await fetch('/stats').then((res) => res.json());
 
-    return {
-      props: {
-        topScorers: stats.slice(0, 3),
-        rows: stats.slice(3).map((stat) => ({
-          ...stat,
-          username: stat.username.split('#')[0],
-          id: stat.username,
-        })),
-      },
+        return {
+            props: {
+                topScorers: stats.slice(0, 3),
+                rows: stats.slice(3).map((stat) => ({
+                    ...stat,
+                    username: stat.username.split('#')[0],
+                    id: stat.username,
+                })),
+            },
+        };
     };
-  };
 </script>
 
 <script>
-  import 'carbon-components-svelte/css/g90.css';
-  import { DataTable } from 'carbon-components-svelte';
-  import Card from '../components/Card.svelte';
+    import 'carbon-components-svelte/css/g90.css';
+    import {DataTable} from 'carbon-components-svelte';
+    import Card from '../components/Card.svelte';
+    import Header from '../components/Header.svelte';
 
-  const headers = [
-    {
-      key: 'username',
-      value: 'Username',
-    },
-    {
-      key: 'count',
-      value: 'Count',
-    },
-  ];
+    const headers = [
+        {
+            key: 'username',
+            value: 'Username',
+        },
+        {
+            key: 'count',
+            value: 'Count',
+        },
+    ];
 
-  export let rows;
-  export let topScorers;
+    export let rows;
+    export let topScorers;
 </script>
+<style>
+    .top-scores {
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+    }
+</style>
 
-<Card data={topScorers} />
-<DataTable {headers} {rows} />
+<Header/>
+<div class="top-scores">
+    <Card data={topScorers}/>
+</div>
+<DataTable {headers} {rows}/>
