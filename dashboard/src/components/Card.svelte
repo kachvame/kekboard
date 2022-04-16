dead86d969fc7ce1186e13d8dc634ebed3f10504
invakid404 <invakid404@riseup.net>
2022-04-16 19:21:06 +0300

    chore: run prettier

--- dashboard/src/components/Card.svelte
+++ dashboard/src/components/Card.svelte
@@ -1,70 +1,69 @@
 <script>
-    export let data;
-
+  export let data;
 </script>
+
 <div class="wrapper">
-    {#each data as {username, count, avatar}, index}
-        <div class="card-body">
-            <div class="crown-{index}"></div>
-            <div class="avatar">
-                <img class="avatar" src="{avatar}" alt="{username}">
-            </div>
-            <h2 class="card-title">{username.split('#')[0]}</h2>
-            <div class="card-count">{count}</div>
-        </div>
-    {/each}
+  {#each data as { username, count, avatar }, index}
+    <div class="card-body">
+      <div class="crown-{index}" />
+      <div class="avatar">
+        <img class="avatar" src={avatar} alt={username} />
+      </div>
+      <h2 class="card-title">{username.split('#')[0]}</h2>
+      <div class="card-count">{count}</div>
+    </div>
+  {/each}
 </div>
 
 <style>
-    .wrapper {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        align-items: center;
-        margin: 0 auto;
-        width: 100%;
-    }
+  .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    width: 100%;
+  }
 
-    .avatar {
-        width: 3rem;
-        height: 3rem;
-        border-radius: 50%;
-    }
+  .avatar {
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+  }
 
-    .card-body {
-        position: relative;
-        padding: 1rem;
-        height: 100%;
-        width: 30%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+  .card-body {
+    position: relative;
+    padding: 1rem;
+    height: 100%;
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
-    .card-count::before {
-        content: "Kek count: ";
-        display: inline-block;
-        font-weight: 500;
-    }
+  .card-count::before {
+    content: 'Kek count: ';
+    display: inline-block;
+    font-weight: 500;
+  }
 
-    .card-count {
-        font-size: 1.2rem;
-        font-weight: 700;
+  .card-count {
+    font-size: 1.2rem;
+    font-weight: 700;
 
-        width: fit-content;
-        margin: 0 auto auto;
-    }
+    width: fit-content;
+    margin: 0 auto auto;
+  }
 
-    .crown-0 {
-        position: absolute;
-        top: 0.2rem;
-        left: 50%;
-        right: 50%;
-        transform: translate(-50%, -50%);
-        width: 2rem;
-        height: 2rem;
-        background-image: url(https://svgheart.com/wp-content/uploads/2020/10/-252.png);
-        background-size: 2rem;
-    }
-
+  .crown-0 {
+    position: absolute;
+    top: 0.2rem;
+    left: 50%;
+    right: 50%;
+    transform: translate(-50%, -50%);
+    width: 2rem;
+    height: 2rem;
+    background-image: url(https://svgheart.com/wp-content/uploads/2020/10/-252.png);
+    background-size: 2rem;
+  }
 </style>
--- dashboard/src/routes/index.svelte
+++ dashboard/src/routes/index.svelte
@@ -1,39 +1,39 @@
 <script context="module">
-    export const load = async ({fetch}) => {
-        const stats = await fetch('/stats').then((res) => res.json());
+  export const load = async ({ fetch }) => {
+    const stats = await fetch('/stats').then((res) => res.json());
 
-        return {
-            props: {
-                topScorers: stats.slice(0, 3),
-                rows: stats.slice(3).map((stat) => ({
-                    ...stat,
-                    username: stat.username.split('#')[0],
-                    id: stat.username,
-                })),
-            },
-        };
+    return {
+      props: {
+        topScorers: stats.slice(0, 3),
+        rows: stats.slice(3).map((stat) => ({
+          ...stat,
+          username: stat.username.split('#')[0],
+          id: stat.username,
+        })),
+      },
     };
+  };
 </script>
 
 <script>
-    import 'carbon-components-svelte/css/g90.css';
-    import {DataTable} from 'carbon-components-svelte';
-    import Card from '../components/Card.svelte';
+  import 'carbon-components-svelte/css/g90.css';
+  import { DataTable } from 'carbon-components-svelte';
+  import Card from '../components/Card.svelte';
 
-    const headers = [
-        {
-            key: 'username',
-            value: 'Username',
-        },
-        {
-            key: 'count',
-            value: 'Count',
-        },
-    ];
+  const headers = [
+    {
+      key: 'username',
+      value: 'Username',
+    },
+    {
+      key: 'count',
+      value: 'Count',
+    },
+  ];
 
-    export let rows;
-    export let topScorers;
-
+  export let rows;
+  export let topScorers;
 </script>
-<Card data={topScorers}/>
-<DataTable {headers} {rows}/>
+
+<Card data={topScorers} />
+<DataTable {headers} {rows} />
