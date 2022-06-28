dead447001998dbcbf19f0fbb6efb392d6c510ec
invakid404 <invakid404@riseup.net>
2022-06-28 18:43:43 +0300

    chore(dashboard): use incrementing counters for scoped CSS classes

--- dashboard/svelte.config.js
+++ dashboard/svelte.config.js
@@ -2,6 +2,16 @@ import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 import { optimizeImports } from 'carbon-preprocess-svelte';
 
+const counters = {};
+
+const increment = (id) => {
+  if (!counters[id]) {
+    counters[id] = 0;
+  }
+
+  return counters[id]++;
+};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -10,6 +20,10 @@ const config = {
 
   kit: {
     adapter: adapter(),
+  },
+
+  compilerOptions: {
+    cssHash: ({ hash, css, name, filename }) => `${name}-${increment(name)}`,
   },
 };
 
